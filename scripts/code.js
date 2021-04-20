5246c7a6af6fc96710138c3d66d8920128b2d33d
AndresFts <76638493+AndresFts@users.noreply.github.com>
2021-04-19 21:42:59 -0400

    Add files via upload

--- scripts/code.js
+++ scripts/code.js
@@ -1,14 +1,29 @@
+const menu = document.querySelector(".menu-toggle");
+const links = document.querySelector(".nav__links");
+
 window.addEventListener('load', function(){
-    new Glider(document.querySelector('.slider__lista'), {
+    new Glider(document.querySelector('.destacados__lista'), {
         slidesToShow: 'auto',
         itemWidth: 325,
         slidesToScroll: 3,
-        dots: '.slider__indicadores',
+        dots: '.destacados__indicadores',
         arrows: {
-            prev: '.slider__anterior',
-            next: '.slider__siguiente'
+            prev: '.destacados__anterior',
+            next: '.destacados__siguiente'
         }
     });
+});
+
+window.addEventListener("load", function(){
+    menu.onclick = ()=>{
+        links.classList.toggle("active");
+        if(links.classList.contains("active")){
+            menu.innerHTML = `<i class="far fa-window-close" style="solid" aria-hidden="true"></i>`;
+        }
+        else{
+            menu.innerHTML = `<i class="fa fa-bars" aria-hidden="true"></i>`
+        }
+    }
 });
 
 // delPDC = ()=> {
