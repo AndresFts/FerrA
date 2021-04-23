76b751ea6a691d49d9c0f9527c9c91726d182b45
AndresFts <76638493+AndresFts@users.noreply.github.com>
2021-04-23 10:28:00 -0400

    Add files via upload

--- scripts/code.js
+++ scripts/code.js
@@ -1,11 +1,8 @@
-const menu = document.querySelector(".menu-toggle");
-const links = document.querySelector(".nav__links");
-
 window.addEventListener('load', function(){
     new Glider(document.querySelector('.destacados__lista'), {
         slidesToShow: 'auto',
         itemWidth: 325,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         dots: '.destacados__indicadores',
         arrows: {
             prev: '.destacados__anterior',
@@ -13,24 +10,3 @@ window.addEventListener('load', function(){
         }
     });
 });
-
-window.addEventListener("load", function(){
-    menu.onclick = ()=>{
-        links.classList.toggle("active");
-        if(links.classList.contains("active")){
-            menu.innerHTML = `<i class="far fa-window-close" style="solid" aria-hidden="true"></i>`;
-        }
-        else{
-            menu.innerHTML = `<i class="fa fa-bars" aria-hidden="true"></i>`
-        }
-    }
-});
-
-// delPDC = ()=> {
-//     document.querySelector("body").removeChild(document.getElementById("pantallaDeCarga"));
-// }
-
-// window.onload = ()=>{
-//     document.getElementById("pantallaDeCarga").classList.add("desaparecer"),
-//     setTimeout(delPDC, 2000)
-// };
