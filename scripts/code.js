43db22d5d19be22d972afaeefd26dcedc0b544f0
AndresFts <76638493+AndresFts@users.noreply.github.com>
2021-05-12 21:37:38 -0400

    Add files via upload

--- scripts/code.js
+++ scripts/code.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function(){
     new Glider(document.querySelector('.destacados__lista'), {
         slidesToShow: 'auto',
         itemWidth: 325,
-        slidesToScroll: 1,
+        slidesToScroll: 3,
         dots: '.destacados__indicadores',
         arrows: {
             prev: '.destacados__anterior',
